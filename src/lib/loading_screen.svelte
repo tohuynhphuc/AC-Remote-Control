bf7541612b3fef2cbb4988c0a948aef903c23271
lts20050703 <lts20050703@gmail.com>
2024-10-30 17:42:26 -0700

    2024.10.30.0: Loading Screen: Loading animation

--- src/lib/loading_screen.svelte
+++ src/lib/loading_screen.svelte
@@ -12,10 +12,13 @@
 	<div
 		class="fixed inset-0 z-50 flex flex-col items-center justify-center gap-4 bg-base-100 bg-opacity-95 p-1 text-center"
 	>
-		<div class="bg-base-100 p-1 text-7xl">Loading save...</div>
+		<span class="loading loading-spinner loading-lg"></span>
+		<div class="bg-base-100 p-1 text-5xl">Loading save...</div>
 		<div class="w-96 max-w-full bg-base-100 p-1">
 			It shouldn't take more than 1 second to load save, if it does then it's likely that Link have
-			updated the save format. Press the button below to clear the save
+			updated the save format.
+			<br />
+			Press the button below to clear the save
 		</div>
 		<button class="btn btn-error" onclick={clear_save}>Clear save</button>
 	</div>
@@ -23,9 +26,11 @@
 	<div
 		class="fixed inset-0 z-50 flex flex-col items-center justify-center gap-4 bg-base-100 bg-opacity-95 p-1 text-center"
 	>
-		<div class="bg-base-100 p-1 text-7xl text-error">Loading save failed!</div>
+		<div class="bg-base-100 p-1 text-5xl text-error">Loading save failed!</div>
 		<div class="w-96 max-w-full bg-base-100 p-1">
-			It's likely that Link have updated the save format. Press the button below to clear the save
+			It's likely that Link have updated the save format.
+			<br />
+			Press the button below to clear the save
 		</div>
 		<button class="btn btn-error" onclick={clear_save}>Clear save</button>
 	</div>
