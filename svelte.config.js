6e2a2e1c38ff6ed84ccbe22b5ee9f856b8294566
Link <link20050703@gmail.com>
2024-12-23 00:51:58 -0800

    2024.12.23.0: Update svelte.config.js - Enable runes mode by default

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,8 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 import adapter from "svelte-adapter-bun"
 /** @type {import('@sveltejs/kit').Config} */
-export default { preprocess: vitePreprocess(), kit: { adapter: adapter() } }
+export default {
+	preprocess: vitePreprocess(),
+	kit: { adapter: adapter() },
+	compilerOptions: { runes: true }
+}
