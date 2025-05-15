65e96e9660e539a99e21618be7fb2932470dbf38
tohuynhphuc <140296933+tohuynhphuc@users.noreply.github.com>
2025-05-15 16:02:23 +0700

    Basic UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let ac_state = $state<string>('default');
+	let hour = $state<number>(0);
+	let minute = $state<number>(0);
+	let use_timer = $state<boolean>(false);
+</script>
+
+<select bind:value={ac_state} class="select select-primary">
+	<option value="default">Default: 27C, Fan: Auto, Direction: Auto</option>
+	<option value="quick_cooling">Quick Cooling: 25C, Fan: Strong, Direction: Auto</option>
+	<option value="low_power">Low Power: 28C, Fan: Weak, Direction: Top</option>
+	<option value="off">Off</option>
+</select>
+
+<div class="mt-2 flex items-center gap-2">
+	<input type="checkbox" bind:checked={use_timer} class="toggle toggle-primary" />
+	<span>Use Timer</span>
+</div>
+
+<div class="flex flex-col gap-2">
+	<p>Hour:</p>
+	<input bind:value={hour} class="input input-bordered w-full" type="number" min="0" />
+</div>
+
+<div class="flex flex-col gap-2">
+	<p>Minute:</p>
+	<input bind:value={minute} class="input input-bordered w-full" type="number" min="0" max="59" />
+</div>
+
+<button
+	class="btn btn-primary mt-4"
+	onclick={async () => {
+		fetch('', {
+			method: 'PUT',
+			body: JSON.stringify(ac_state),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+	}}
+>
+	Send Command
+</button>
